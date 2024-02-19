517141bc9e5e001c8b1be0394dbf1fc8e46f12c0
pmedam <142865611+pmedam@users.noreply.github.com>
2024-02-18 19:48:55 -0600

    Working on Contact Add form

--- routes/index.js
+++ routes/index.js
@@ -16,5 +16,21 @@ router.get('/contact_list', function(req, res, next) {
   res.render('contact_list', {title: "All Contacts!", contacts: data, errors: null});
 });
 
+/* GET new contact. */
+router.get('/contact_add',
+body('firstName', 'First Name must not be empty.').notEmpty(),
+body('lastName', 'Last Name must not be empty.').notEmpty(),
+body('email', 'Email must not be empty.').notEmpty(),
+function (req, res, next){
+  const results = validationResult(req);
+
+  if(!results.isEmpty()){
+    res.render('contact_add', {title: "Add Contact", errors: results.array()});
+  }
+  else{
+    contactsRepo.create(new Contact('', req.body.firstName, req.body.lastName, req.body.email, req.body.notes));
+    res.direct('/contact_list');
+  }
+});
 
 module.exports = router;
